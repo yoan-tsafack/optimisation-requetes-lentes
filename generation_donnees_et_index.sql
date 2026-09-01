dc92f96010de3bafbd26e893b1b56712ef534c5c
yoan-tsafack <yoan.tsafack21@gmail.com>
2026-09-01 02:51:16 +0200

    Mise a jour des commentaires du script SQL

--- generation_donnees_et_index.sql
+++ generation_donnees_et_index.sql
@@ -1,4 +1,4 @@
--- Génère 30 000 commandes réparties entre 2020 et 2026
+ --Génération de données de test 
 INSERT INTO commandes (id_fournisseur, date_commande, statut)
 SELECT
     (floor(random() * 4) + 1)::int,
@@ -6,7 +6,6 @@ SELECT
     (ARRAY['livree','en_attente','annulee'])[floor(random() * 3 + 1)]
 FROM generate_series(1, 30000);
 
--- Génère 2 à 4 lignes de commande pour chacune des commandes créées
 INSERT INTO lignes_commande (id_commande, id_produit, quantite, prix_unitaire)
 SELECT
     c.id_commande,
@@ -15,17 +14,17 @@ SELECT
     round((random() * 50000 + 500)::numeric, 2)
 FROM commandes c
 CROSS JOIN generate_series(1, (floor(random() * 3) + 2)::int)
-WHERE c.id_commande > 4;  -- on garde tes 4 commandes d'origine intactes
+WHERE c.id_commande > 4;
 
 SELECT COUNT(*) FROM commandes;
 SELECT COUNT(*) FROM lignes_commande;
 
+--recherche par produit 
 EXPLAIN ANALYZE
 SELECT lc.*, p.nom_produit
 FROM lignes_commande lc
 JOIN produits p ON p.id_produit = lc.id_produit
 WHERE lc.id_produit = 5;
-
 
 CREATE INDEX idx_lignes_commande_id_produit ON lignes_commande(id_produit);
 
@@ -35,6 +34,7 @@ FROM lignes_commande lc
 JOIN produits p ON p.id_produit = lc.id_produit
 WHERE lc.id_produit = 5;
 
+--recherche par commande  
 EXPLAIN ANALYZE
 SELECT * FROM lignes_commande WHERE id_commande = 15000;
 
